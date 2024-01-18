a392225559e54dcf294b611b25bd9db33f5ee533
adamwang15 <adamwang15@gmail.com>
2024-01-18 18:05:36 +1100

    update dependencies version

--- R/bsvarSIGNs-package.R
+++ R/bsvarSIGNs-package.R
@@ -1,21 +1,21 @@
 #' @title Bayesian Estimation of Structural Vector Autoregressive Models Identified by Sign and Narrative Restrictions
 #'
-#' @description Implements efficient algorithms for the Bayesian estimation of 
-#' Stuructural Vector Autoregressive models identified by sign and narrative 
-#' restrictions following Rubio-Ramírez, Waggoner & Zha (2010) 
-#' <doi:10.1111/j.1467-937X.2009.00578.x> and Antolín-Díaz & Rubio-Ramírez (2018) 
+#' @description Implements efficient algorithms for the Bayesian estimation of
+#' Stuructural Vector Autoregressive models identified by sign and narrative
+#' restrictions following Rubio-Ramírez, Waggoner & Zha (2010)
+#' <doi:10.1111/j.1467-937X.2009.00578.x> and Antolín-Díaz & Rubio-Ramírez (2018)
 #' <doi:10.1257/aer.20161852>.
 # #' @details
 #' @name bsvarSIGNs-package
 #' @aliases bsvarSIGNs-package bsvarSIGNs
-#' @docType package
+#' "_PACKAGE"
 #' @useDynLib bsvarSIGNs, .registration = TRUE
 #' @importFrom R6 R6Class
 #' @importFrom Rcpp sourceCpp
 #' @import bsvars
 #' @import RcppArmadillo
 #' @import RcppProgress
-#' @note This package is currently in active development. We give no 
+#' @note This package is currently in active development. We give no
 #' warranty that anything here works.
 #' @author Xiaolei Wang \email{adamwang15@gmail.com} Tomasz Woźniak \email{wozniak.tom@pm.me}
 #' @references
