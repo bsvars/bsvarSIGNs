9ff96bc07cb9b1269cc79c4ca61d9792409f2538
Tomasz Woźniak <wozniak.tom@pm.me>
2024-03-18 11:22:09 +1100

    updated simple test #9

--- tests/testthat/test-specify.R
+++ tests/testthat/test-specify.R
@@ -1,9 +1,9 @@
-data(oil)
-spec = specify_bsvarSIGN$new(oil)
 
 test_that(
   "minimum example of specify_bsvarSIGN works", 
   {
+    data(oil)
+    spec = specify_bsvarSIGN$new(oil)
     expect_identical(
       class(spec)[1],
       "BSVARSIGN"
