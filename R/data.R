66a8d5c23032847975f045265aba7e0a1a345b75
adamwang15 <adamwang15@gmail.com>
2024-04-22 17:03:52 +1000

    optimism shock replication

--- R/data.R
+++ R/data.R
@@ -19,3 +19,31 @@
 #' @source
 #' Replication package, \url{https://www.aeaweb.org/articles?id=10.1257/aer.20161852}
 "oil"
+
+
+
+#' @title A 5-variable US business cycle data, from 1955 Q1 to 2004 Q4
+#'
+#' @description A sample data to identify optimism shock.
+#'
+#' @usage data(optimism)
+#'
+#' @format A matrix and a \code{ts} object with time series of over two hundred observations on 5 variables:
+#' \describe{
+#'   \item{productivity}{quarterly factor-utilization-adjusted total factor productivity}
+#'   \item{stock_prices}{quarterly end-of-period S&P 500 divided by CPI}
+#'   \item{consumption}{quarterly real consumption expenditures on nondurable goods and services}
+#'   \item{real_interest_rate}{quarterly real interest rate}
+#'   \item{hours_worked}{quarterly hours of all persons in the non-farm business sector}
+#' }
+#'
+#' The series are as described by Beaudry, Nam and Wang (2011) in section 2.2.
+#'
+#' @references
+#' Arias, Jonas E., Juan F. Rubio‐Ramírez, and Daniel F. Waggoner. "Inference based on structural vector autoregressions identified with sign and zero restrictions: Theory and applications." Econometrica 86, no. 2 (2018): 685-720. <doi:10.3982/ECTA14468>
+#' 
+#' Beaudry, Paul, Deokwoo Nam, and Jian Wang. Do mood swings drive business cycles and is it rational?. No. w17651. National Bureau of Economic Research, 2011. <doi:10.3386/w17651>
+#'
+#' @source
+#' Replication package, \url{https://www.econometricsociety.org/publications/econometrica/2018/03/01/inference-based-structural-vector-autoregressions-identified}
+"optimism"
--- README.md
+++ README.md
@@ -14,7 +14,7 @@ devtools::install_github("bsvars/bsvarSIGNs")
 
 # Example
 
-A repliaction of Antolín-Díaz & Rubio-Ramírez (2018).
+A repliaction of Antolín-Díaz and Rubio-Ramírez (2018).
 
 ``` r
 data(oil)
@@ -31,6 +31,28 @@ specification  = specify_bsvarSIGN$new(oil,
 posterior      = estimate(specification, S = 100)
 irf            = compute_impulse_responses(posterior, horizon = 24)
 plot(irf)
+```
+
+A repliaction of Arias, Rubio-Ramírez and Waggoner (2018).
+
+``` r
+data(optimism)
+
+# try ?specify_bsvarSIGN
+zero_irf          = matrix(0, nrow = 5, ncol = 5)
+zero_irf[1, 1]    = 1
+sign_irf          = array(0, dim = c(5, 5, 1))
+sign_irf[2, 1, 1] = 1
+
+specification  = specify_bsvarSIGN$new(optimism*100,
+                                       p = 4,
+                                       sign_irf = sign_irf,
+                                       zero_irf = zero_irf
+                                       )
+
+posterior      = estimate(specification, S = 10000)
+irf            = compute_impulse_responses(posterior, horizon = 40)
+plot(irf, probability = 0.68)
 ```
 
 <!-- badges: start -->
