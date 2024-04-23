e42d48b04a9834cacfc044e85daec62a3fe47973
Tomasz Woźniak <wozniak.tom@pm.me>
2024-04-23 15:57:55 +1000

    various versions of riw sampler #18

--- inst/varia/nicetry.cpp
+++ inst/varia/nicetry.cpp
@@ -16,40 +16,67 @@ using namespace arma;
 // }
 
 // [[Rcpp::export]]
-arma::mat matrwish_byT1 (
-    const arma::mat&  S, 
-    const double&     nu
+arma::cube rIW_byT1 (
+    const int         n, // a positive int
+    const arma::mat&  S, // an NxN positive definite matrix
+    const double&     nu // a positive double
 ) {
-  mat sigma   = iwishrnd(S, nu);
+  int N         = S.n_cols;
+  cube sigma(N, N, n);
+  mat S_inv_chol = chol(inv_sympd(S));
+  for (int i=0; i<n; i++) {
+    sigma.slice(i)   = iwishrnd(S, nu, S_inv_chol);
+  } // END i loop
   return sigma;
 }
 
 // [[Rcpp::export]]
-arma::mat matrwish_byT2 (
-    const arma::mat&  S, 
-    const double&     nu
+arma::cube rIW_byT1b (
+    const int         n, // a positive int
+    const arma::mat&  S, // an NxN positive definite matrix
+    const double&     nu // a positive double
+) {
+  int N         = S.n_cols;
+  cube sigma(N, N, n);
+  for (int i=0; i<n; i++) {
+    sigma.slice(i)   = iwishrnd(S, nu);
+  } // END i loop
+  return sigma;
+}
+
+
+
+// [[Rcpp::export]]
+arma::cube rIW_byT2 (
+    const int         n, // a positive int
+    const arma::mat&  S, // an NxN positive definite matrix
+    const double&     nu // a positive double
 ) {
   // Based on algorithm B.4.4. from Appendinx B by Bauwens, Lubrano, Richard (1999) Bayesian Inference in Dynamic Econometric Models, Oxford Uni Press
   
   int N           = S.n_cols;
-
   mat s_chol      = chol(S, "lower");
+  cube sigma(N, N, n);
 
-  mat Q(N, N, fill::zeros);
-  Q.diag()        = sqrt(pow(chi2rnd(nu - N + 1, N), -1));
+  for (int i=0; i<n; i++) {
+    mat Q(N, N, fill::zeros);
+    Q.diag()        = sqrt(pow(chi2rnd(nu - N + 1, N), -1));
+    
+    for (int i = 0; i < (N - 1); i++) {
+      Q.submat(i + 1, i, N - 1, i) = randn(N - i - 1);
+    }
+    mat Q_inv       = inv(trimatu(Q));
+    sigma.slice(i)  = s_chol * Q_inv.t() * Q_inv * s_chol.t();
+  } // END i loop
   
-  for (int i = 0; i < (N - 1); i++) {
-    Q.submat(i + 1, i, N - 1, i) = randn(N - i - 1);
-  }
-  mat Q_inv       = inv(trimatu(Q));
-  
-  return s_chol * Q_inv.t() * Q_inv * s_chol.t();
+  return sigma;
 }
 
 
 
 /*** R
-N = 20
+n = 1000000
+N = 2
 # K = 3
 
 set.seed(12)
@@ -71,12 +98,26 @@ nu = N + 1
 #   times = 100000
 # )  
 
-rbenchmark::benchmark(
-  matrwish_byT1(S, nu),
-  matrwish_byT2(S, nu),
-  replications = 100000
-)
-  
+# rbenchmark::benchmark(
+#   rIW_byT1(n, S, nu),
+#   rIW_byT2(n, S, nu),
+#   replications = 1000
+# )
+
+set.seed(12)
+ssT1 = rIW_byT1(n, S, nu)
+# set.seed(12)
+ssT1b = rIW_byT1b(n, S, nu)
+# set.seed(12)
+ssT2 = rIW_byT2(n, S, nu)
+
+apply(ssT1, 1:2, mean)
+apply(ssT1b, 1:2, mean)  
+apply(ssT2, 1:2, mean)  
+
+apply(ssT1, 1:2, sd)
+apply(ssT1b, 1:2, sd)  
+apply(ssT2, 1:2, sd)  
 */
 
 
