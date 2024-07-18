e7ac40de35b16fcf416624fd46a91e2e5299fa52
adamwang15 <adamwang15@gmail.com>
2024-07-18 16:48:53 +1000

    Revert "include ifdef"
    This reverts commit cb20a106d1ee396bfe1d14ac3365f2f19cf4545e.

--- src/bsvars_sign.cpp
+++ src/bsvars_sign.cpp
@@ -3,19 +3,7 @@
 #include "progress.hpp"
 #include "Rcpp/Rmath.h"
 #include <bsvars.h>
-
-#ifdef _OPENMP
-  #include <omp.h>
-#else
-  // for machines with compilers void of openmp support
-  #define omp_get_num_threads()  1
-  #define omp_get_thread_num()   0
-  #define omp_get_max_threads()  1
-  #define omp_get_thread_limit() 1
-  #define omp_get_num_procs()    1
-  #define omp_set_nested(a)   // empty statement to remove the call
-  #define omp_get_wtime()        0
-#endif
+// #include <omp.h>
 
 #include "sample_hyper.h"
 #include "sample_Q.h"
@@ -58,16 +46,16 @@ Rcpp::List bsvar_sign_cpp(
   }
   
   // Progress bar setup
-  double num_threads;
-  #pragma omp parallel
-  {
-    num_threads = omp_get_num_threads();
-  }
+  double num_threads = 1;
+  // #pragma omp parallel
+  // {
+  //   num_threads = omp_get_num_threads();
+  // }
   vec prog_rep_points = arma::round(arma::linspace(0, S / num_threads, 50));
   if (show_progress) {
     Rcout << "**************************************************|" << endl;
-    Rcout << " bsvarSIGNs: Bayesian Structural VAR with sign,   |" << endl;
-    Rcout << "             zero and narrative restrictions      |" << endl;
+    Rcout << " bsvarSIGNs: Bayesian Structural VAR with zero,   |" << endl;
+    Rcout << "             sign and narrative restrictions      |" << endl;
     Rcout << "**************************************************|" << endl;
     // Rcout << " Gibbs sampler for the SVAR model                 |" << endl;
     // Rcout << "**************************************************|" << endl;
@@ -95,45 +83,53 @@ Rcpp::List bsvar_sign_cpp(
   
   int        S_hyper  = hypers.n_cols - 1;
   int        prior_nu = as<int>(prior["nu"]);
+  int        post_nu  = prior_nu + T;
   
+  double     w, mu, delta, lambda;
+  
+  vec        hyper, psi;
   vec        prior_v = as<mat>(prior["V"]).diag();
   
+  mat        B, Sigma, chol_Sigma, h_invp, Q, shocks;
+  mat        prior_V, prior_S, post_B, post_V, post_S;
+  mat        Ystar, Xstar, Yplus, Xplus;
   mat        prior_B = as<mat>(prior["B"]);
   mat        Ysoc    = as<mat>(prior["Ysoc"]);
   mat        Xsoc    = as<mat>(prior["Xsoc"]);
   mat        Ysur    = as<mat>(prior["Ysur"]);
   mat        Xsur    = as<mat>(prior["Xsur"]);
   
-  #pragma omp parallel for
+  field<mat> result;
+  
+  // #pragma omp parallel for private(hyper, mu, delta, lambda, psi, prior_V, prior_S, Ystar, Xstar, Yplus, Xplus, result, post_B, post_V, post_S, Sigma, chol_Sigma, B, h_invp, Q, shocks, w)
   for (int s = 0; s < S; s++) {
     
-    vec    hyper      = hypers.col(randi(distr_param(0, S_hyper)));
-    double mu         = hyper(0);
-    double delta      = hyper(1);
-    double lambda     = hyper(2);
-    vec    psi        = hyper.rows(3, N + 2);
+    hyper      = hypers.col(randi(distr_param(0, S_hyper)));
+    mu         = hyper(0);
+    delta      = hyper(1);
+    lambda     = hyper(2);
+    psi        = hyper.rows(3, N + 2);
     
     // update Minnesota prior
-    mat    prior_V    = diagmat(prior_v % join_vert(lambda * lambda * repmat(1 / psi, p, 1),
-                                                    ones<vec>(K - N * p)));
-    mat    prior_S    = diagmat(psi);
+    prior_V    = diagmat(prior_v %
+      join_vert(lambda * lambda * repmat(1 / psi, p, 1),
+                ones<vec>(K - N * p)));
+    prior_S    = diagmat(psi);
     
     // update dummy observation prior
-    mat    Ystar      = join_vert(Ysoc / mu, Ysur / delta);
-    mat    Xstar      = join_vert(Xsoc / mu, Xsur / delta);
-    mat    Yplus      = join_vert(Ystar, Y);
-    mat    Xplus      = join_vert(Xstar, X);
+    Ystar      = join_vert(Ysoc / mu, Ysur / delta);
+    Xstar      = join_vert(Xsoc / mu, Xsur / delta);
+    Yplus      = join_vert(Ystar, Y);
+    Xplus      = join_vert(Xstar, X);
     
     // posterior parameters
-    field<mat> result = niw_cpp(Yplus, Xplus, prior_B, prior_V, prior_S, prior_nu);
-    mat    post_B     = result(0);
-    mat    post_V     = result(1);
-    mat    post_S     = result(2);
-    int    post_nu    = as_scalar(result(3));
+    result     = niw_cpp(Yplus, Xplus, prior_B, prior_V, prior_S, prior_nu);
+    post_B     = result(0);
+    post_V     = result(1);
+    post_S     = result(2);
+    post_nu    = as_scalar(result(3));
     
-    double w          = 0;
-    
-    mat    Sigma, chol_Sigma, B, h_invp, Q, shocks;
+    w          = 0;
     
     while (w == 0) {
       
@@ -145,8 +141,8 @@ Rcpp::List bsvar_sign_cpp(
       B          = rmatnorm_cpp(post_B, post_V, Sigma);
       h_invp     = inv(trimatl(chol_Sigma)); // lower tri, h(Sigma) is upper tri
       
-      result     = sample_Q(p, Y, X, B, h_invp, chol_Sigma, prior, VB, 
-                            sign_irf, sign_narrative, sign_B, Z, max_tries);
+      result     = sample_Q(p, Y, X, B, h_invp, chol_Sigma, prior, 
+                            VB, sign_irf, sign_narrative, sign_B, Z, max_tries);
       Q          = result(0);
       shocks     = result(1);
       w          = as_scalar(result(2));
@@ -161,9 +157,16 @@ Rcpp::List bsvar_sign_cpp(
     posterior_Theta0.slice(s) = chol_Sigma * Q;
     posterior_shocks.slice(s) = shocks;
     
-    if (omp_get_thread_num() == 0 and any(prog_rep_points == s)) {
-      bar.increment();
-    }
+    // Increment progress bar
+    if (any(prog_rep_points == s)) bar.increment();
+    
+    // if (omp_get_thread_num() == 0) {
+    //   // Check for user interrupts
+    //   if (s % 10 == 0) checkUserInterrupt();
+    // 
+    //   // Increment progress bar
+    //   if (any(prog_rep_points == s)) bar.increment();
+    // }
     
   } // END s loop
   
